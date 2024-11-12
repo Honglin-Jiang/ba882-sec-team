a29b7d656b13e8f9f7c261602e5b7164eed7f512
Unice-YuFang <unice2006@gmail.com>
2024-11-12 05:52:58 +0000

    Update Prefect auto run

--- flows/deploy-etl.py
+++ flows/deploy-etl.py
@@ -10,10 +10,10 @@ if __name__ == "__main__":
     etl_deployment.deploy(
         name="ba882-team9-deployment-lab6",
         work_pool_name="ba882-team9-schedule",
-        cron="50 5 * * *",
+        cron="55 5 * * *",
         tags=["daily-run"],
         description="Pipeline to extract data from YFinance API and MD&A filing, transform, and store it into Motherduck DB",
         version="1.0.0",
         job_variables={
-            "pip_packages": ["pandas", "requests", "google-cloud-secret-manager", "google"]}
+            "pip_packages": ["pandas", "requests","google-cloud", "google-cloud-secret-manager"]}
     ) 
