dae4ccf0b083d2d2418f95937d990005de8bcc82
Unice-YuFang <unice2006@gmail.com>
2024-11-12 06:05:39 +0000

    Update Prefect auto run

--- flows/deploy-etl.py
+++ flows/deploy-etl.py
@@ -1,4 +1,7 @@
+'''
 from prefect import flow
+from prefect.deployments import Deployment
+from flows.etl import etl_flow 
 
 if __name__ == "__main__":
     etl_deployment = flow.from_source(
@@ -17,3 +20,26 @@ if __name__ == "__main__":
         job_variables={
             "pip_packages": ["pandas", "requests","google-cloud", "google-cloud-secret-manager"]}
     ) 
+'''
+
+from prefect.deployments import Deployment
+from flows.etl import etl_flow 
+
+# Define the deployment
+etl_deployment = Deployment.build_from_flow(
+    flow=etl_flow,
+    name="ba882-team9-deployment-lab6",
+    work_pool_name="ba882-team9-schedule",
+    schedule={
+        "cron": "10 6 * * *"  # Cron schedule for deployment
+    },
+    tags=["daily-run"],
+    description="Pipeline to extract data from YFinance API and MD&A filing, transform, and store it into Motherduck DB",
+    version="1.0.0",
+    job_variables={
+        "pip_packages": ["pandas", "requests", "google-cloud-secret-manager"]
+    }
+)
+
+if __name__ == "__main__":
+    etl_deployment.apply()  # Apply the deployment configuration
