56b2ee717293aaa9c19ea75e43db28afce6e6918
Unice-YuFang <unice2006@gmail.com>
2024-12-05 04:55:50 +0000

    Update GenAI text to sql code.

--- genai/streamlit/app.py
+++ genai/streamlit/app.py
@@ -3,6 +3,6 @@ import streamlit as st
 st.set_page_config(page_title="BA882 Team9 GenAI Apps", layout="wide")
 
 pg = st.navigation([
-    st.Page("assistants.py", title="Simple Chat Assistant", icon=":material/chat:")
+    st.Page("text_to_sql.py", title="Simple Chat Assistant", icon=":material/chat:")
     ])
 pg.run()
