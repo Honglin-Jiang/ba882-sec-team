dd6af4959fc027fb922726a8249f472908714350
Unice-YuFang <unice2006@gmail.com>
2024-12-06 05:43:34 +0000

    Add daily news sentiment and stock analysis, and news topic modeling analysis. Closes #75 #76.

--- genai/streamlit/app.py
+++ genai/streamlit/app.py
@@ -4,6 +4,8 @@ st.set_page_config(page_title="BA882 Team9 GenAI Apps", layout="wide")
 
 pg = st.navigation([
     st.Page("text_to_sql.py", title="AI-Driven Data Query and Visualization Tool", icon=":material/chat:"),
-    st.Page("sentiment_driven_stock_insights.py", title="MD&A Sentiment and Stock Price Trends", icon="📄")
+    st.Page("yearly_mda_sentiment_stock_insights.py", title="MD&A Sentiment and Stock Price Trends", icon="📄"),
+    st.Page("daily_news_sentiment_stock_analysis.py", title="Daily News Sentiment and Stock Price Trends", icon="📈"),
+    st.Page("news_topic_modeling_analysis.py", title="News Topic Modeling Analysis", icon="🗂️")
     ])
 pg.run()
